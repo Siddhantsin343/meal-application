aad23014c9d59e35bfa2fe7b08382c59471d3ca9
siddhant <siddhantsin343@gmail.com>
2025-11-09 22:01:47 +0530

    - remove all stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./compontent/Header";
 
-console.log(React);
+const App = ()=>(
+    <>
+        <Header/>
+    </>
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(<App/>)
+const application = createBrowserRouter([])
+root.render(<RouterProvider router={application}></RouterProvider>)
